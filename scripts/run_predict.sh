a77f73bf1c565f7e5ec0b0b68b67f7b693638cc5
cmdm-jia-cheng <jchong@cmdm.csie.ntu.edu.tw>
2025-07-29 10:17:47 +0800

    Delete scripts directory

--- scripts/run_predict.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python chempropIRZenodo/chempropIR/predict.py \
-  --qnn \
-  --test_path ./data/research_data/experiment/test_smiles.csv \
-  --checkpoint_path ./output/model/ffnn_direct/fold_0/model_0/model.pt \
-  --preds_path ./output/model/ffnn_direct/fold_0/ffnn_direct.csv
